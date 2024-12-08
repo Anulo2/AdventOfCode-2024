f1cc6daf6cd66f49446fd16bf870271828a4c282
_Zaizen_ <leonardocanello@protonmail.com>
2024-12-08 14:16:14 +0100

    Day 8: better looping logic

--- src/8/index.ts
+++ src/8/index.ts
@@ -1,41 +1,26 @@
 export function part1(input: string): number {
-	const { frequencies, lines } = parseInput(input);
-	const antinodes = calculateAntinodes(frequencies, lines, false);
+	const { frequencies, rows, cols } = parseInput(input);
+	const antinodes = calculateAntinodes(frequencies, rows, cols, false);
 	//printMapWithAntinodes(frequencies, lines, antinodes);
 	return antinodes.size;
 }
 
 export function part2(input: string): number {
-	const { frequencies, lines } = parseInput(input);
-	const antinodes = calculateAntinodes(frequencies, lines, true);
+	const { frequencies, rows, cols } = parseInput(input);
+	const antinodes = calculateAntinodes(frequencies, rows, cols, true);
 	//printMapWithAntinodes(frequencies, lines, antinodes);
 	return antinodes.size;
 }
 
-function printMapWithAntinodes(
-	frequencies: { [key: string]: [number, number][] },
-	lines: string[],
-	antinodes: Set<string>,
-): void {
-	const map = lines.map((line) => line.split(""));
-	for (const [char, value] of Object.entries(frequencies)) {
-		for (const [x, y] of value) {
-			map[y][x] = char;
-		}
-	}
-	for (const antinode of antinodes) {
-		const [x, y] = antinode.split(",").map(Number);
-		map[y][x] = "#";
-	}
-	console.log(map.map((line) => line.join("")).join("\n"));
-}
-
 function parseInput(input: string): {
 	frequencies: { [key: string]: [number, number][] };
-	lines: string[];
+	rows: number;
+	cols: number;
 } {
 	const frequencies: { [key: string]: [number, number][] } = {};
 	const lines = input.trim().split("\r\n");
+	const rows = lines.length;
+	const cols = lines[0].length;
 
 	lines.forEach((line, y) => {
 		line.split("").forEach((char, x) => {
@@ -48,12 +33,13 @@ function parseInput(input: string): {
 		});
 	});
 
-	return { frequencies, lines };
+	return { frequencies, rows, cols };
 }
 
 function calculateAntinodes(
 	frequencies: { [key: string]: [number, number][] },
-	lines: string[],
+	rows: number,
+	cols: number,
 	includeIntermediate: boolean,
 ): Set<string> {
 	const antinodes = new Set<string>();
@@ -68,29 +54,39 @@ function calculateAntinodes(
 
 				const antinode1X = x1 - dx;
 				const antinode1Y = y1 - dy;
-				if (antinodesWithinBounds(antinode1X, antinode1Y, lines)) {
+				if (antinodesWithinBounds(antinode1X, antinode1Y, rows, cols)) {
 					antinodes.add(`${antinode1X},${antinode1Y}`);
 				}
 
 				const antinode2X = x2 + dx;
 				const antinode2Y = y2 + dy;
-				if (antinodesWithinBounds(antinode2X, antinode2Y, lines)) {
+				if (antinodesWithinBounds(antinode2X, antinode2Y, rows, cols)) {
 					antinodes.add(`${antinode2X},${antinode2Y}`);
 				}
 
 				if (includeIntermediate) {
-					for (let k = 0; k < 100; k++) {
+					let outOfBounds1 = false;
+					let outOfBounds2 = false;
+
+					let k = 0;
+					while (!outOfBounds1 || !outOfBounds2) {
 						const antinodeX1 = x1 + k * dx;
 						const antinodeY1 = y1 + k * dy;
-						if (antinodesWithinBounds(antinodeX1, antinodeY1, lines)) {
+						if (antinodesWithinBounds(antinodeX1, antinodeY1, rows, cols)) {
 							antinodes.add(`${antinodeX1},${antinodeY1}`);
+						} else {
+							outOfBounds1 = true;
 						}
 
 						const antinodeX2 = x1 - k * dx;
 						const antinodeY2 = y1 - k * dy;
-						if (antinodesWithinBounds(antinodeX2, antinodeY2, lines)) {
+						if (antinodesWithinBounds(antinodeX2, antinodeY2, rows, cols)) {
 							antinodes.add(`${antinodeX2},${antinodeY2}`);
+						} else {
+							outOfBounds2 = true;
 						}
+
+						k++;
 					}
 				}
 			}
@@ -100,6 +96,11 @@ function calculateAntinodes(
 	return antinodes;
 }
 
-function antinodesWithinBounds(x: number, y: number, lines: string[]): boolean {
-	return x >= 0 && x < lines[0].length && y >= 0 && y < lines.length;
+function antinodesWithinBounds(
+	x: number,
+	y: number,
+	rows: number,
+	cols: number,
+): boolean {
+	return x >= 0 && x < cols && y >= 0 && y < rows;
 }
